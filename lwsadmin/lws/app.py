18f958be04af021646f7278300a1cf4b4980d14b
lza_menace <lza_menace@protonmail.com>
2023-11-08 18:27:53 -0800

    update defaults

--- lwsadmin/lws/app.py
+++ lwsadmin/lws/app.py
@@ -1,4 +1,4 @@
 from lws.factory import create_app
 from lws import config
 
-run = create_app().run(host=config.HOST)
+run = create_app().run(host=config.HOST, port=config.PORT)
--- lwsadmin/lws/config.py
+++ lwsadmin/lws/config.py
@@ -7,10 +7,11 @@ if DEBUG:
     load_dotenv()
 
 HOST = env.get("HOST", "127.0.0.1")
+PORT = env.get("PORT", 5000)
 TEMPLATES_AUTO_RELOAD = True
 QUART_ENV = env.get("QUART_ENV", "development")
 SECRET_KEY = env.get("SECRET_KEY", token_urlsafe(12))
-SERVER_NAME = env.get("SERVER_NAME", "http://127.0.0.1:5000")
+SERVER_NAME = env.get("SERVER_NAME", f"127.0.0.1:${PORT}")
 QUART_AUTH_DURATION = int(env.get('QUART_AUTH_DURATION', 60 * 60))    # 1 hour
 
 # LWS
